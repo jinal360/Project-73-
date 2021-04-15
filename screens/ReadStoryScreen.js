ca1825bec9a5b7a74fec20c1c438ddac6f86d9ce
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-04-15 18:01:51 +0530

    Update ReadStoryScreen.js

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -7,7 +7,7 @@ export default class ReadStoryScreen extends React.Component{
     constructor(props){
       super(props)
       this.state = {
-        allTransactions: []
+        allStories: []
       }
     }
     
@@ -20,7 +20,7 @@ export default class ReadStoryScreen extends React.Component{
   };
 
      componentDidMount = async ()=>{
-      const query = await db.collection("transactions").get()
+      const query = await db.collection("Story").get()
       query.docs.map((doc) =>{
         this.setState ({
           allTransactions:[...this.state.allStories,doc.data()]
@@ -40,12 +40,12 @@ export default class ReadStoryScreen extends React.Component{
         value={search}
       />
          <FlatList
-         data = {this.state.allTransactions}
+         data = {this.state.allStories}
          renderItem = {({item})=>(
           <View style ={{borderBottomWidth :2}}>
-                <Text>{"BookId:"+ transaction.bookId}</Text>
-                <Text>{"StudentId:"+ transaction.studentId}</Text>
-                <Text>{"TransactionId:"+ transaction.transactionId}</Text>
+                <Text>{"Title:"+ story.bookId}</Text>
+                <Text>{"Author:"+ story.studentId}</Text>
+                <Text>{"StoryId:"+ story.transactionId}</Text>
             </View>
          )}
          keyExtractor ={(item,index)=>index.toString()}
